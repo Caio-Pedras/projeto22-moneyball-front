7726da00e44cc3ce3ab221f0132b7de759a03025
CaioPedras <caio.vp@gmail.com>
2022-10-10 16:48:32 -0300

    chore: create user context

--- src/userContext/UserContextProvider.js
+++ src/userContext/UserContextProvider.js
@@ -2,7 +2,11 @@ import React from "react";
 import { UserContext } from "./userContext.js";
 // import { useState } from "react";
 export default function UserContextProvider(props) {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = process.env.REACT_APP_URL;
   return (
-    <UserContext.Provider value={{}}>{props.children}</UserContext.Provider>
+    <UserContext.Provider value={{ URL, API_KEY }}>
+      {props.children}
+    </UserContext.Provider>
   );
 }
