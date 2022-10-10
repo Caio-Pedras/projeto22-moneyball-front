cb9cf3c99548f610332646b70504b894a68bf76f
CaioPedras <caio.vp@gmail.com>
2022-10-10 17:28:48 -0300

    feat: singup page

---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,131 @@
+import { SignUpBody, LeftSize, RightSize, Title, Text, Form } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Oval } from "react-loader-spinner";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export default function SignUpPage() {
+  const URL = "http://localhost:5000"; // alterar pra context depois
+  const navigate = useNavigate();
+  const [cadastroData, setCadastroData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [enableButton, setEnableButton] = useState(true);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!enableButton) return;
+
+    setEnableButton(false);
+    const response = axios.post(`${URL}/signup`, cadastroData);
+
+    response
+      .then(() => {
+        setTimeout(() => navigate("/login"), 2600);
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title:
+            "You have registed with sucess! You will redirect to login page",
+          showConfirmButton: false,
+          timer: 2500,
+          background: "#333333",
+          color: "#ffffff",
+        });
+      })
+      .catch((err) => {
+        let erro = err.response.data;
+
+        Swal.fire({
+          icon: "error",
+          title: erro,
+          text: "Try with another name",
+          confirmButtonColor: "#1877F2",
+          background: "#333333",
+          color: "#ffffff",
+        });
+        setEnableButton(true);
+      });
+  }
+
+  return (
+    <SignUpBody>
+      <LeftSize>
+        <Text>
+          <Title>MoneyBall</Title>
+        </Text>
+      </LeftSize>
+      <RightSize>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="email"
+            placeholder="exmploe_email@gmail.com"
+            value={cadastroData.email}
+            functionOnChange={(e) =>
+              setCadastroData({ ...cadastroData, email: e.target.value })
+            }
+            id="email"
+            autocomplete="off"
+            pattern="^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$"
+            errorMessage={<p>Type a valid email</p>}
+            // autoFocus={true}
+          >
+            e-mail
+          </Input>
+          <Input
+            type="password"
+            placeholder="Senh4_Fort3"
+            value={cadastroData.password}
+            functionOnChange={(e) =>
+              setCadastroData({ ...cadastroData, password: e.target.value })
+            }
+            id="senha"
+            autocomplete="off"
+            pattern="^.{3,30}$"
+            errorMessage={<p>Type a valid password</p>}
+          >
+            password
+          </Input>
+          <Input
+            type="password"
+            placeholder="Senh4_Fort3"
+            value={cadastroData.confirmPassword}
+            functionOnChange={(e) =>
+              setCadastroData({
+                ...cadastroData,
+                confirmPassword: e.target.value,
+              })
+            }
+            id="confirmarsenha"
+            autocomplete="off"
+            pattern="^.{3,30}$"
+            errorMessage={<p>Type a valid password</p>}
+          >
+            confirm password
+          </Input>
+
+          <Button width="429px">
+            {enableButton ? (
+              <p>Sign up</p>
+            ) : (
+              <Oval
+                ariaLabel="loading-indicator"
+                height={30}
+                width={30}
+                strokeWidth={7}
+                color="#ffffff"
+                secondaryColor="#9f9f9f"
+              />
+            )}
+          </Button>
+        </Form>
+        <Link to="/login">Switch back to log in</Link>
+      </RightSize>
+    </SignUpBody>
+  );
+}
---
+++ src/pages/SignUpPage/styles.js
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+
+export const SignUpBody = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+
+  @media (max-width: 500px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+`;
+
+export const LeftSize = styled.div`
+  background-color: #0d1724;
+  width: 66%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 500px) {
+    width: 100%;
+    height: 175px;
+  }
+`;
+
+export const RightSize = styled.div`
+  background-color: #f2f2f2;
+  width: 34%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  a {
+    color: #0d1724;
+    text-decoration: underline;
+    outline: currentColor;
+  }
+
+  @media (max-width: 500px) {
+    width: 100%;
+    height: calc(100% - 175px);
+    justify-content: flex-start;
+    padding-top: 15px;
+  }
+`;
+
+export const Text = styled.div`
+  width: 28em;
+  height: 100%;
+  word-break: break-word;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 2em;
+
+  @media (max-width: 500px) {
+    width: 237px;
+  }
+`;
+
+export const Title = styled.div`
+  width: 100%;
+  font-family: "Passion One";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 84px;
+  line-height: 117px;
+  letter-spacing: 0.05em;
+  color: #ffffff;
+
+  @media (max-width: 500px) {
+    font-size: 76px;
+    line-height: 84px;
+    text-align: center;
+  }
+`;
+
+export const SubTitle = styled.div`
+  font-family: "Oswald";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 34px;
+  line-height: 64px;
+
+  color: #ffffff;
+
+  @media (max-width: 500px) {
+    font-size: 23px;
+    line-height: 34px;
+    text-align: center;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 28rem;
+  padding: 0 15px;
+  button {
+    margin: 20px 0;
+    padding: 10px 0;
+
+    p {
+      margin: 3px 0;
+    }
+  }
+
+  @media (max-width: 500px) {
+    height: 85%;
+
+    button {
+      margin: 10px 0;
+    }
+  }
+`;
