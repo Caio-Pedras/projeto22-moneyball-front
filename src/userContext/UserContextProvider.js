db921b5ccb62188a7494f988c07fd2f034e88338
CaioPedras <caio.vp@gmail.com>
2022-10-10 17:29:09 -0300

    chore: add usestate for userToken on context

--- src/userContext/UserContextProvider.js
+++ src/userContext/UserContextProvider.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { UserContext } from "./userContext.js";
 // import { useState } from "react";
 export default function UserContextProvider(props) {
   const API_KEY = process.env.REACT_APP_API_KEY;
   const URL = process.env.REACT_APP_URL;
+  const [userToken, setUserToken] = useState();
   return (
-    <UserContext.Provider value={{ URL, API_KEY }}>
+    <UserContext.Provider value={{ URL, API_KEY, userToken, setUserToken }}>
       {props.children}
     </UserContext.Provider>
   );
