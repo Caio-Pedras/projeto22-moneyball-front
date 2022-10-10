5421d05910e236a7cb41f0bf0e8d3b1199174c9d
CaioPedras <caio.vp@gmail.com>
2022-10-10 17:27:38 -0300

    feat: add auth routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
-// import LoginPage from "./pages/LoginPage";
-// import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 import MainPage from "./pages/MainPage";
 import TeamPage from "./pages/TeamPage";
 import "./assets/css/reset.css";
@@ -13,8 +13,8 @@ export default function App() {
     <UserContextProvider>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/login" element={<LoginPage />} /> */}
-          {/* <Route path="/signup" element={<SignUpPage />} /> */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
           <Route path="/" element={<MainPage />} />
           <Route path="/team/:teamId" element={<TeamPage />} />
         </Routes>
